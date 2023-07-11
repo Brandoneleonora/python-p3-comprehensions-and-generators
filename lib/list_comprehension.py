968341afb29d5ec7d91e77889aa535b913960705
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-07-10 20:45:35 -0400

    complete project

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [n for n in num_list if n % 2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) == 0:
+        return sentence_list
+    else:
+        exlclamation_list = [f"{s}!" for s in sentence_list]
+        return exlclamation_list
+    
